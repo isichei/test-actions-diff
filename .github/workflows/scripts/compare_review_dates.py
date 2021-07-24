b92097cc8c3aaa804f95039d125defbc57111af3
k1br <22888464+K1Br@users.noreply.github>
2021-07-24 23:24:30 +0100

    Fixed env vars

--- .github/workflows/scripts/compare_review_dates.py
+++ .github/workflows/scripts/compare_review_dates.py
@@ -5,4 +5,4 @@ print(os.getenv('GITHUB_NEW_DATE'))
 
 date_diff = dt.strptime(os.environ['GITHUB_NEW_DATE'], "%Y-%m-%d") - dt.strptime(os.environ['GITHUB_OLD_DATE'], "%Y-%m-%d")
 print(date_diff)
-if data_diff.days > 92: raise ValueError("date diff to high")
+if date_diff.days > 92: raise ValueError("date diff to high")
